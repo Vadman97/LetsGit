9102e115818feec74171feffd617b9389a307729
Vadim Korolik <vkorolik@gmail.com>
2015-01-24 11:52:54 -0800

    Trying to get git downloader working

--- git-download.js
+++ git-download.js
@@ -1,4 +1,7 @@
-var spawn = require('child_process').spawn,
-        child = spawn("git pull", ""),
+function run_cmd(cmd, args, cb, end) {
+    var spawn = require('child_process').spawn,
+        child = spawn(cmd, args),
         me = this;
-
+    child.stdout.on('data', function (buffer) { cb(me, buffer) });
+    child.stdout.on('end', end);
+}
