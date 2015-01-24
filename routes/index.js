776b144cb073695005a794ba419194aec4ca6ff6
Niraj Pant <nirajpant7@gmail.com>
2015-01-24 15:57:41 -0800

    add login + register routes

--- routes/index.js
+++ routes/index.js
@@ -3,4 +3,10 @@ exports.addRoutes = function(app) {
 	app.get('/', function(req, res){
 		res.render('index', { body: 'This is LetsGit' });
 	});
+	app.get('/login', function(req, res){
+                res.render('login', { body: 'This is LetsGit' });
+        });
+	app.get('/register', function(req, res){
+                res.render('register', { body: 'This is LetsGit' });
+        });
 };
