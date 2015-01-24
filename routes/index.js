a055523d36a7483845c74b469ccaafe47e9e6861
Bogdan Vitoc <bogdanvitoc@gmail.com>
2015-01-24 12:25:16 -0800

    switch jade ==> ejs

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@
  */
 
 exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+  res.render('index', { body: 'This is LetsGit' });
 };
 
 exports.gitPull = function(req, res) {
