8123b4013ba61ffbee2f2b9b186a167252712341
Bogdan Vitoc <bogdanvitoc@gmail.com>
2015-01-24 14:22:59 -0800

    no message

--- git-download.js
+++
@@ -1,7 +0,0 @@
-function run_cmd(cmd, args, cb, end) {
-    var spawn = require('child_process').spawn,
-        child = spawn(cmd, args),
-        me = this;
-    child.stdout.on('data', function (buffer) { cb(me, buffer) });
-    child.stdout.on('end', end);
-}
