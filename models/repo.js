816e60aa10944aed6b01cc7b4c9e49d1108c3a4d
Vadim Korolik <vkorolik@gmail.com>
2015-01-25 01:06:13 -0800

    no unique

--- models/repo.js
+++ models/repo.js
@@ -2,8 +2,8 @@ var passport = require('passport'),
     mongoose = require('mongoose');
 
 var repoSchema = mongoose.Schema({
-  	name: { type: String, required: true, unique: true},
-  	path: { type: String, required: true, uniquue: true},
+  	name: { type: String, required: true},
+  	path: { type: String, required: true},
  	createdAt: { type: Date, required: true},
  	updatedAt: { type: Date, required: true},
  	userId: { type: mongoose.Schema.Types.ObjectId, required: true}
