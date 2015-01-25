dacada8b01680d903c517ada3430ca019f37f3c0
Niraj Pant <nirajpant7@gmail.com>
2015-01-24 23:08:50 -0800

    aws stuff

--- routes/git.js
+++ routes/git.js
@@ -2,7 +2,9 @@ var nodegit = require('nodegit');
 var promisify = require("promisify-node");
 var fse = promisify(require("fs-extra"));
 var validator = require('validator');
-var path = "../repos/test";
+var fs = require('fs');
+var AWS = require('aws-sdk');
+AWS.config.region = 'us-west-1';
 
 exports.addRoutes = function(app) {
 	app.get('/app', function(req, res) {
@@ -10,22 +12,22 @@ exports.addRoutes = function(app) {
 	});
 
 	app.post('/clone', function(req, res) {
+		// var path = "/tmp/" + req.user._id + ;
+		var path = "/tmp/asd";
 		var repoURL = req.body.url;
 			
-		// clone(repoURL, "../repos").then(function(repo) {
-
 		fse.remove(path).then(function() {
 			var entry;
 
 			nodegit.Clone.clone(
-				"https://github.com/nodegit/nodegit.git",
+				"https://github.com/sathyasom/test.git",
 				path,
 				{ ignoreCertErrors: 1})
 			  	.then(function(repo) {
-				return repo.getCommit("59b20b8d5c6ff8d09518454d4dd8b7b30f095ab5");
+				return repo.getCommit("571da7b66408b8a5839eb5682856f846386514a3");
 			  })
 			  .then(function(commit) {
-				return commit.getEntry("README.md");
+				return commit.getEntry("test.txt");
 			  })
 			  .then(function(entryResult) {
 				entry = entryResult;
@@ -37,6 +39,20 @@ exports.addRoutes = function(app) {
 				var firstTenLines = blob.toString().split("\n").slice(0, 10).join("\n");
 				console.log(firstTenLines);
 				console.log("...");
+
+				var s3bucket = new AWS.S3({params: {Bucket: 'letsgit'}});
+				s3bucket.createBucket(function() {
+				  var params = {Key: 'myKey', Body: 'Hello!'};
+				  s3bucket.upload(params, function(err, data) {
+				    if (err) {
+				      console.log("Error uploading data: ", err);
+				    } else {
+				      console.log("Successfully uploaded data to myBucket/myKey");
+				    }
+				  });
+				});
+
+				res.json({message: "test"});
 		});
 					   
 		});
