423a57523daa66fe78750ceb9eafbbec95294d98
Greg Carlin <gregthegeek@optonline.net>
2015-01-24 18:54:49 -0800

    Update package.json and routes/file.js

--- routes/file.js
+++ routes/file.js
@@ -2,6 +2,6 @@ require('./common');
 
 exports.addRoutes = function(app) {
   app.get('/file', function(req, res){
-    render('file', { js: ['ace/ace', 'file'] }, res);
+    renderDashboard('file', { js: ['ace/ace', 'file'] }, res);
   });
 };
