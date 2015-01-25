817196c1f0dce01ecf61816540acbe1053710f5b
Vadim Korolik <vkorolik@gmail.com>
2015-01-24 23:34:27 -0800

    Added project repo loading from mongo backend

--- routes/project.js
+++ routes/project.js
@@ -1,7 +1,18 @@
 require('./common');
+var Repo = require('../models/repo.js');
 
 exports.addRoutes = function(app) {
-  app.get('/project/:id', function(req, res){
-    renderDashboard('project', { css: ['project'] }, res);
+  app.get('/project/:id', ensureAuthenticated, function(req, res){
+  	Repo.findOne({_id: req.param("id")}, function(error, data){
+  		if (error)
+  			res.redirect("/dashboard");
+  		console.log(data);
+		renderDashboard('project', {css:["dashboard"], project: data}, res);
+    });
   });
 };
+
+function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) { return next(); }
+  res.redirect('/')
+}
