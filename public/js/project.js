7dd570a2983b1af2467218171ea17d349cf66be5
Greg Carlin <gregthegeek@optonline.net>
2015-01-25 04:44:33 -0800

    Upload file stuff

--- public/js/project.js
+++ public/js/project.js
@@ -1,9 +1,8 @@
-$(document).ready(function() {
-  $('#uploadZip').on('submit', function(e) {
-    $.post('/uploadRepo', new FormData(this), function(data, status){
-      //Success handler
-      console.log("submited!");
-    });
-    e.preventDefault();
-  });
+$('#uploadZip .btn').change(function() {
+  $.post('/uploadRepo', $('#uploadZip').serialize(), function(data) {
+    location.reload();
+  }, 'json');
+  $('label.addButton').html('');
+  $('label.addButton').removeClass(); // removes all classes
+  $('.row label').addClass('fa fa-refresh fa-lg fa-spin');
 });
