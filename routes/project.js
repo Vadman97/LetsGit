968050e9c37645968dac2a1fefd4e7896930a98a
Greg Carlin <gregthegeek@optonline.net>
2015-01-25 12:38:12 -0800

    Does this work

--- routes/project.js
+++ routes/project.js
@@ -61,7 +61,8 @@ exports.addRoutes = function(app) {
     Repo.findOne({_id: req.param("id")}, function(error, data){
       if (error || data == null)//user doesnt have repo
         res.redirect("/dashboard");
-      var pathString = data.path + req.params[0]; // todo ensure this doesn't access weird places, actually no slash at end
+      //var pathString = data.path + req.params[0]; // todo ensure this doesn't access weird places, actually no slash at end
+      var pathString = "/project/" + req.param("id") + '/' + req.params[0];
       var repoName = data.name;
       console.log("DEEP PATH STRING: " + pathString);
       fs.stat(pathString, function(err, stats) {
@@ -102,7 +103,8 @@ exports.addRoutes = function(app) {
 	  			//console.log("Printing project backend stuff");
 	  			//console.log(pathString);
 	  			//console.log(files);
-	  			renderDashboard('project', {css:["dashboard"], js:["project", "projectButtons"], project: data, ownerID: req.user._id, files: goodFiles, mts: modTimes, currentPath: "/project/" + req.param("id"), repoName: repoName, parent: false}, res);
+                renderDashboard('project', {css:["dashboard"], js:["project", "projectButtons"], project: data, ownerID: req.user._id, files: goodFiles, mts: modTimes, currentPath: "/project/" + req.param("id") + '/' + req.params[0], repoName: repoName, parent: false}, res);
+
 	  		});
 
          	/*fs.readdir(pathString, function(error, files) {
