77a1d34d7b868d0c64103a1f7c78f200e27925a7
Vadim Korolik <vkorolik@gmail.com>
2015-01-25 11:54:47 -0800

    Works?

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -5,3 +5,12 @@ $('#upload-submit').click(function() {
   }, 'json');
   $('#addRepositoryForm').parent().html('This may take a while...<div class="progress"><div class="progress-bar progress-bar-striped active" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width: 100%"></div></div>');
 });
+
+$(document).ready(function() {
+  $(window).keydown(function(event){
+    if(event.keyCode == 13) {
+      event.preventDefault();
+      return false;
+    }
+  });
+});
