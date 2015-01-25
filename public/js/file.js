ed0d8a2be3906c8e504f617c051dad2a66512b5d
Greg Carlin <gregthegeek@optonline.net>
2015-01-24 17:16:01 -0800

    Setting up read and write modes in ace

--- public/js/file.js
+++ public/js/file.js
@@ -4,3 +4,13 @@ editor.getSession().setMode("ace/mode/javascript");
 editor.setAutoScrollEditorIntoView(true);
 editor.setOption("maxLines", 30);
 editor.setOption("minLines", 2);
+editor.setShowPrintMargin(false);
+
+// val=true to set read only, val=false for write mode
+function setReadOnly(val) {
+  editor.setReadOnly(val);
+  editor.setHighlightActiveLine(!val); // don't highlight active line
+  editor.getSession().setUseWorker(!val); // disable warnings
+  editor.setShowFoldWidgets(!val); // don't fold code
+  $('.ace_scroller').css('background-color', val ? '#eee' : 'inherit');
+}
