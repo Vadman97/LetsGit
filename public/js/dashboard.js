98d6a5363e2dcdec2f75e3b16c99f427d03c3c17
Vadim Korolik <vkorolik@gmail.com>
2015-01-25 12:06:29 -0800

    test

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -6,11 +6,11 @@ $('#upload-submit').click(function() {
   $('#addRepositoryForm').parent().html('This may take a while...<div class="progress"><div class="progress-bar progress-bar-striped active" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width: 100%"></div></div>');
 });
 
-/*$(document).ready(function() {
+$(document).ready(function() {
   $(window).keydown(function(event){
     if(event.keyCode == 13) {
       event.preventDefault();
       return false;
     }
   });
-});*/
+});
