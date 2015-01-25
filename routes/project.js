f98f64c1dcabe7fc4a9d617b0d963533885db710
Vadim Korolik <vkorolik@gmail.com>
2015-01-25 01:58:39 -0800

    Working on project backend

--- routes/project.js
+++ routes/project.js
@@ -7,12 +7,30 @@ exports.addRoutes = function(app) {
   	Repo.findOne({_id: req.param("id")}, function(error, data){
   		if (error || data == null)//user doesnt have repo
   			res.redirect("/dashboard");
-  		var pathString = "./repos";
-  		fs.readdir(pathString, function(error, files){
+  		var pathString = data.path;
+  		fs.readdir(pathString, function(error, files)
+  		{
+  			//for now, project figures out if directory or file when requesting
+  			/*
+  			var links = [];
+  			for (i in files)
+  			{
+  				//pass to ejs file names, links (different if file or folder
+  				//no slash at the end of the pathString actually
+  				//make the ejs display folders vs files differently
+  				if (fs.statSync(pathString + files[i]).isDirectory())
+  				{
+  					links[i] = "/project/" + req.param("id") + "/" + files[i];
+  				}
+  				else
+  				{
+  					links[i] = "/project/" + req.param("id") + "/" + files[i];
+  				}
+  			}*/
   			console.log("Printing project backend stuff");
   			console.log(pathString);
   			console.log(files);
-			renderDashboard('project', {css:["dashboard"], project: data, files: null}, res);
+			renderDashboard('project', {css:["dashboard"], project: data, files: files, currentPath: "/project/" + req.param("id")}, res);
   		});
     });
   });
