633d743e5c9b0f6b1d5e449e02899715b9564a5b
James Anderson <me@jamesaanderson.com>
2015-01-24 16:58:53 -0800

    Fixed auth

---
+++ models/user.js
@@ -0,0 +1,31 @@
+var passport = require('passport'),
+    mongoose = require('mongoose'),
+    bcrypt = require('bcrypt');
+
+var userSchema = mongoose.Schema({
+  email: { type: String, required: true, unique: true},
+  password: { type: String, required: true},
+});
+
+userSchema.pre('save', function(next) {
+  var user = this;
+  if (!user.isModified('password')) return next();
+  bcrypt.genSalt(5, function(err, salt) {
+    if (err) return next(err);
+    bcrypt.hash(user.password, salt, null, function(err, hash) {
+      if (err) return next(err);
+      user.password = hash;
+      next();
+    });
+  });
+});
+
+userSchema.methods.comparePassword = function(candidatePassword, cb) {
+  bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
+    if (err) return cb(err);
+    cb(null, isMatch);
+  });
+};
+
+
+module.exports = mongoose.model('User', userSchema);
---
+++ routes/signup.js
@@ -0,0 +1,20 @@
+var User = require('../models/user.js');
+
+exports.addRoutes = function(app) {
+  app.post('/signup', function(req, res) {
+      user = User({email: req.body.email, password: req.body.password});
+
+    User.findOne({email: req.body.email}, function(err, existingUser) {
+      if (existingUser) {
+        res.redirect('/signup');
+      }
+      user.save(function(err) {
+        if (err) return next(err);
+        req.logIn(user, function(err) {
+          if (err) return next(err);
+          res.redirect('/');
+        });
+      });
+    });
+  });
+}
