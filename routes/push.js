df1f7a6a62684fcfd65266a8322ae564cf1f151b
Vadim Korolik <vkorolik@gmail.com>
2015-01-25 15:05:38 -0800

    debug messages

--- routes/push.js
+++ routes/push.js
@@ -5,15 +5,18 @@ var fse = promisify(require('fs-extra'));
 var Repo = require('../models/repo.js');
 
 exports.addRoutes = function(app) {
+	console.log("starting push!");
   app.get('/pushRepo/:id', function(req, res) {
     var repo = Repo.findOne({_id: req.param('id')}, function(error, data) {
       nodegit.Repository.open(path.resolve(__dirname + '/../' + data.path))
       .then(function(repository) {
         nodegit.Remote.remove(repository, "origin");
+	console.log("returning new repository");
         return nodegit.Remote.create(repository, "origin",
               data.remoteURL);
       })
       .then(function(remote) {
+	console.log("connecting to new repository");
         remote.connect(nodegit.Enums.DIRECTION.PUSH);
 
         var push;
@@ -21,6 +24,7 @@ exports.addRoutes = function(app) {
       // We need to set the auth on the remote, not the push object
       remote.setCallbacks({
         credentials: function(url, userName) {
+	console.log("returning login info");
           return nodegit.Cred.userpassPlaintextNew(req.body.username, req.body.password);
         }
       });
@@ -28,6 +32,7 @@ exports.addRoutes = function(app) {
       // Create the push object for this remote
       return nodegit.Push.create(remote)
       .then(function(pushResult) {
+	console.log("push result: " + pushResult);
         push = pushResult;
 
         // This just says what branch we're pushing onto what remote branch
@@ -36,6 +41,7 @@ exports.addRoutes = function(app) {
         // This is the call that performs the actual push
         return push.finish();
       }).then(function() {
+	console.log("Unpack!");
         // Check to see if the remote accepted our push request.
         return push.unpackOk();
       });
